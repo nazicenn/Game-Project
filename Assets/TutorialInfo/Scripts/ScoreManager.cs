0ad919fd251f50647ed9c5d1cd0e2616c5ceb75b
Naz İçen <icennaz354@gmail.com>
2026-05-22 16:03:02 +0300

    feat: add coin counter to score system

--- Assets/TutorialInfo/Scripts/ScoreManager.cs
+++ Assets/TutorialInfo/Scripts/ScoreManager.cs
@@ -9,9 +9,12 @@ public class ScoreManager : MonoBehaviour
     [Header("UI References")]
     public TextMeshProUGUI distanceText;
     public TextMeshProUGUI highscoreText;
+    public TextMeshProUGUI coinText;
+    public TextMeshProUGUI finalScoreText;
 
     private float distance;
     private int highscore;
+    private int coinCount;
     private bool isGameActive = true;
 
     void Awake()
@@ -28,6 +31,8 @@ public class ScoreManager : MonoBehaviour
         UpdateHighscoreUI();
 
         distance = 0f;
+        coinCount = 0;
+        UpdateCoinUI();
         isGameActive = true;
     }
 
@@ -36,7 +41,6 @@ public class ScoreManager : MonoBehaviour
         if (!isGameActive) return;
 
         distance += GroundSpawner.moveSpeed * Time.deltaTime;
-
         UpdateDistanceUI();
 
         int currentDistanceInt = Mathf.FloorToInt(distance);
@@ -60,24 +64,32 @@ public class ScoreManager : MonoBehaviour
             highscoreText.text = "Best: " + highscore + " m";
     }
 
-    public TextMeshProUGUI finalScoreText; 
+    public void AddCoin()
+    {
+        coinCount++;
+        UpdateCoinUI();
+    }
+
+    void UpdateCoinUI()
+    {
+        if (coinText != null)
+            coinText.text = "Coin: " + coinCount;
+    }
 
     public void GameOver()
     {
         isGameActive = false;
 
         if (finalScoreText != null)
-            finalScoreText.text = "Score: " + Mathf.FloorToInt(distance) + " m";
+            finalScoreText.text = "Score: " + Mathf.FloorToInt(distance) + " m\nCoin: " + coinCount;
     }
 
     public void Restart()
     {
         isGameActive = true;
         distance = 0f;
+        coinCount = 0;
         UpdateDistanceUI();
+        UpdateCoinUI();
     }
-
-
-
 }
-
